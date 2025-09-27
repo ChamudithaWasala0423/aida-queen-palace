f24e578cb8e3d9689961ecfbf5893da702b7bb9a
ChamudithaWasala0423 <chamudithaprabathwasala@gmail.com>
2025-09-27 13:26:19 +0530

    fix: Adjust width of navbar background for better responsiveness

--- src/components/general/navbar.tsx
+++ src/components/general/navbar.tsx
@@ -126,7 +126,7 @@ export default function NavBar() {
             : "-translate-y-full opacity-0 pointer-events-none"
         )}
       >
-        <div className="hidden xl:block m-2 rounded-lg w-[80%] 2xl:w-[60%] bg-black/95 backdrop-blur border-2  shadow-lg border-cyan-500">
+        <div className="hidden xl:block m-2 rounded-lg w-[80%] 2xl:w-[80%] bg-black/95 backdrop-blur border-2  shadow-lg border-cyan-500">
           <div className="mx-auto w-full px-6">
             <div className="flex h-14  items-center justify-center gap-6">
               {/* Left items */}
